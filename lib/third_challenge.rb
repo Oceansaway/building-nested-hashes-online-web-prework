cd858703d679ef35c109ec089c76c99716faf407
Stephanie Purple <spurple133@gmail.com>
2019-03-21 20:05:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,11 +7,10 @@ def third_challenge
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {name: "Romeo", age: "15", status: "alive"  },
+      :patriarch => {name: "Lord Capulet", age: "50"  },
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
-      {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+      :heroine_friends => []
    }
   }
 
